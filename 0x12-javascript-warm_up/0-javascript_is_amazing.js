1135ff5a7192441b15b23f568848b565e644dd75
Mechanic <dumashieanthony@gmail.com>
2024-10-09 10:27:58 +0100

    corrections: first and second challenge;

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,19 +1,8 @@
-// Write a script that prints 3 lines:
+// Write a script that prints “JavaScript is amazing”:
 
-// The first line: “C is fun”
-// The second line: “Python is cool”
-// The third line: “JavaScript is amazing”
+// You must create a constant variable called myVar with the value “JavaScript is amazing”
 // You must use console.log(...) to print all output
 // You are not allowed to use var
 
-function jsIsAmazing(){
-    let line1 = 'C is fun.';
-    let line2 = 'Python is cool.';
-    let line3 = 'JavaScript is amazing.';
-    console.log(line1) ;
-    console.log(line2) ;
-    console.log(line3) ;
-}
-
-jsIsAmazing();
-
+let myVar = "JavaScript is amazing";
+console.log(myVar);

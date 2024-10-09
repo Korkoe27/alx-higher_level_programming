48fd25f9610c6a4de54fcb272099d2d6a5c7f4b8
Mechanic <dumashieanthony@gmail.com>
2024-10-09 10:24:56 +0100

    first challenge

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,19 @@
+// Write a script that prints 3 lines:
+
+// The first line: “C is fun”
+// The second line: “Python is cool”
+// The third line: “JavaScript is amazing”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+
+function jsIsAmazing(){
+    let line1 = 'C is fun.';
+    let line2 = 'Python is cool.';
+    let line3 = 'JavaScript is amazing.';
+    console.log(line1) ;
+    console.log(line2) ;
+    console.log(line3) ;
+}
+
+jsIsAmazing();
+
